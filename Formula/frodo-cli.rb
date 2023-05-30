0d010e61e2da766ad5202764c7dd9f431599396c
Sandeep Chaturvedi <schaturvedi@gmail.com>
2023-05-30 09:51:39 -0600

    Reset tag back after testing

--- Formula/frodo-cli.rb
+++ Formula/frodo-cli.rb
@@ -5,7 +5,7 @@ class FrodoCli < Formula
   homepage "https://github.com/rockcarver/frodo-cli#readme"
   url "https://github.com/rockcarver/frodo-cli.git", 
     branch: "main", 
-    tag: "v0.36"
+    tag: "v0.23.0"
   license "MIT"
   head "https://github.com/rockcarver/frodo-cli.git", branch: "main"
 

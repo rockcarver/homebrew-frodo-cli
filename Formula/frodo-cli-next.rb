be34faa41cb994e70e40f6daef84a6f54da73813
Sandeep Chaturvedi <schaturvedi@gmail.com>
2024-03-22 18:54:56 -0600

    frodo-cli-next 2.0.0-52
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/frodo-cli-next.rb
+++ Formula/frodo-cli-next.rb
@@ -5,7 +5,7 @@ class FrodoCliNext < Formula
   homepage "https://github.com/rockcarver/frodo-cli#readme"
   url "https://github.com/rockcarver/frodo-cli.git",
     branch: "main",
-    tag: "v2.0.0-51"
+    tag: "v2.0.0-52"
   license "MIT"
   head "https://github.com/rockcarver/frodo-cli.git", branch: "main"
 

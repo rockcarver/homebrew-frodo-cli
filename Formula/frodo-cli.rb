40965fa3c0c273d744469d594dc9634bd769ae4e
Sandeep Chaturvedi <schaturvedi@gmail.com>
2024-08-05 13:24:54 -0600

    frodo-cli 2.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/frodo-cli.rb
+++ Formula/frodo-cli.rb
@@ -5,7 +5,7 @@ class FrodoCli < Formula
   homepage "https://github.com/rockcarver/frodo-cli#readme"
   url "https://github.com/rockcarver/frodo-cli.git",
     branch: "main",
-    tag: "v2.0.0"
+    tag: "v2.0.1"
   license "MIT"
   head "https://github.com/rockcarver/frodo-cli.git", branch: "main"
 

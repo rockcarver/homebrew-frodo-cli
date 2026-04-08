a905c7e8a58ab3f58ff52bb78395ba68f078ef89
Volker Scheuber <vscheuber@gmail.com>
2026-04-08 11:03:27 -0600

    frodo-cli-next 4.0.0-43
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/frodo-cli-next.rb
+++ Formula/frodo-cli-next.rb
@@ -5,7 +5,7 @@ class FrodoCliNext < Formula
   homepage "https://github.com/rockcarver/frodo-cli#readme"
   url "https://github.com/rockcarver/frodo-cli.git",
     branch: "main",
-    tag: "v4.0.0-42"
+    tag: "v4.0.0-43"
   license "MIT"
   head "https://github.com/rockcarver/frodo-cli.git", branch: "main"
 

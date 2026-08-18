5523b6ac539fdc512e129935a0ee8bf3ec59d4be
Volker Scheuber <vscheuber@gmail.com>
2026-08-18 07:49:29 -0600

    frodo-cli 4.5.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/frodo-cli.rb
+++ Formula/frodo-cli.rb
@@ -5,7 +5,7 @@ class FrodoCli < Formula
   homepage "https://github.com/rockcarver/frodo-cli#readme"
   url "https://github.com/rockcarver/frodo-cli.git",
     branch: "main",
-    tag: "v4.5.3"
+    tag: "v4.5.4"
   license "MIT"
   head "https://github.com/rockcarver/frodo-cli.git", branch: "main"
 
